93adae8ec2612551734fb1ed69d8c1968bd18d9b
Yudhistira123 <yudhistira.sulaeman@gmail.com>
2025-10-16 07:19:44 +0700

    xx

--- index.mjs
+++ index.mjs
@@ -8,16 +8,16 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-accelerated-2d-canvas",
-      "--no-first-run",
-      "--no-zygote",
-      "--single-process",
-      "--disable-gpu",
-    ],
+    // args: [
+    //   "--no-sandbox",
+    //   "--disable-setuid-sandbox",
+    //   "--disable-dev-shm-usage",
+    //   "--disable-accelerated-2d-canvas",
+    //   "--no-first-run",
+    //   "--no-zygote",
+    //   "--single-process",
+    //   "--disable-gpu",
+    // ],
   },
 });
 
