d0b24e8f872da2f0a5dbc102cdf635fbfb4e5af5
Yudhistira123 <yudhistira.sulaeman@gmail.com>
2025-10-21 06:12:05 +0700

    x

--- handlers/groupMessageHandler.mjs
+++ handlers/groupMessageHandler.mjs
@@ -28,5 +28,63 @@ export default async function groupMessageHandler(client, message) {
     await chat.sendMessage(reply);
   } else if (text.toLowerCase().startsWith("qs:")) {
     await handleQuranCommand(oriText, chat);
+  } else if (text.toLowerCase().startsWith("rn:")) {
+    const indikator = text.toLowerCase().replace("rn:", "").trim();
+    console.log(`🔍 Mencari data rudal untuk: ${indikator}`);
+    if (indikator == "all") {
+      let url = `https://drharryhuiz.my.id/rn01/getMainDataRudal.php`;
+      //   console.log("Fetching data from URL:", url);
+      const response = await axios.get(url);
+      // let kode = response.data.kode_prefix;
+      // let judul = response.data.n_judul;
+      let msg = "";
+      let index = 1;
+      response.data.data.forEach((item) => {
+        msg += index + ". (" + item.kode_prefix + ") " + item.n_judul + "\n";
+        index++;
+      });
+      await chat.sendMessage(msg);
+    } else if (indikator.includes("td")) {
+      let url = `https://drharryhuiz.my.id/rn01/getTDRudalByKode.php?c_kode=${indikator}`;
+      console.log("Fetching data from URL:", url);
+      const response = await axios.get(url);
+      // let kode = response.data.kode_prefix;
+      // let judul = response.data.n_judul;
+      let msg = "";
+      let index = 1;
+      response.data.data.forEach((item) => {
+        msg +=
+          "(" +
+          item.c_kode +
+          ")\n" +
+          item.n_desc.trim().replace(/\n/g, " ") +
+          "*\n\n";
+        index++;
+      });
+      await chat.sendMessage(msg);
+    } else {
+      let url = `https://drharryhuiz.my.id/rn01/getDataRudalByKode.php?c_kode=${indikator}`;
+      //   console.log("Fetching data from URL:", url);
+      const response = await axios.get(url);
+      // let kode = response.data.kode_prefix;
+      // let judul = response.data.n_judul;
+      let msg = "";
+      let index = 1;
+      response.data.data.forEach((item) => {
+        msg +=
+          index +
+          ". (" +
+          item.c_kode +
+          "):" +
+          item.c_revisi +
+          ":" +
+          item.d_appedm +
+          "\n*" +
+          item.n_judul.trim().replace(/\n/g, " ") +
+          "*\n\n";
+        index++;
+      });
+      await chat.sendMessage(msg);
+    }
   }
 }
