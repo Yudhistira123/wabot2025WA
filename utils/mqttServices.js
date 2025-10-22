4f434d369515aa57e45eaece69266f81efedb810
Yudhistira123 <yudhistira.sulaeman@gmail.com>
2025-10-23 06:07:47 +0700

    x

--- utils/mqttServices.js
+++ utils/mqttServices.js
@@ -42,7 +42,7 @@ export async function sendMessages(client, topic, message) {
   //const jids = ["628122132341@c.us", "6285183819833@c.us"]; // your WA targets
   const jids = [
     "628122132341@c.us",
-    "6285183819833@c.us",
+    // "6285183819833@c.us",
     // "6285220757725@c.us",
     // "628122233610@c.us",
     // "6285975386345@c.us",
@@ -59,7 +59,7 @@ export async function sendMessages(client, topic, message) {
       console.log(`✅ Sent to ${jid}: ${text}`);
       //   console.log(`✅ Message sent to ${number}`);
     } catch (err) {
-      console.error(`❌ Failed to send to ${number}:`, err);
+      console.error(`❌ Failed to send to :`, err);
     }
   }
 }
