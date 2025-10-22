2802e8ee4c23d6456c9f2c72bcaacddfdbf27ccc
Yudhistira123 <yudhistira.sulaeman@gmail.com>
2025-10-23 06:10:52 +0700

    x

--- utils/mqttServices.js
+++ utils/mqttServices.js
@@ -38,28 +38,46 @@ export function publishMessage(topic, msg) {
 }
 
 export async function sendMessages(client, topic, message) {
-  const text = `Lampu ${topic} : ${message.toString()}`;
-  //const jids = ["628122132341@c.us", "6285183819833@c.us"]; // your WA targets
-  const jids = [
-    "628122132341@c.us",
-    // "6285183819833@c.us",
-    // "6285220757725@c.us",
-    // "628122233610@c.us",
-    // "6285975386345@c.us",
-    // "628121462983@c.us",
-  ];
+  // const text = `Lampu ${topic} : ${message.toString()}`;
+  // //const jids = ["628122132341@c.us", "6285183819833@c.us"]; // your WA targets
+  // const jids = [
+  //   "628122132341@c.us",
+  //   // "6285183819833@c.us",
+  //   // "6285220757725@c.us",
+  //   // "628122233610@c.us",
+  //   // "6285975386345@c.us",
+  //   // "628121462983@c.us",
+  // ];
+  // for (const jid of jids) {
+  //   try {
+  //     await client.sendMessage(jid, { text }); // <-- FIX: must be { text: "..." }
+
+  //     // await client.sendMessage(
+  //     //   number,
+  //     //   ` Lampu ${topic} : ${message.toString()}`
+  //     // );
+  //     console.log(`✅ Sent to ${jid}: ${text}`);
+  //     //   console.log(`✅ Message sent to ${number}`);
+  //   } catch (err) {
+  //     console.error(`❌ Failed to send to :`, err);
+  //   }
+  // }
+
+  const payload = JSON.parse(message.toString()); // parse MQTT message
+  const text = `Nama: ${payload.name}
+No HP: ${payload.no_hp}
+Alat: ${payload.nama_alat}
+Status: ${payload.status}
+Tanggal: ${payload.tanggal}`;
+
+  const jids = ["628122132341@c.us"];
+
   for (const jid of jids) {
     try {
-      await client.sendMessage(jid, { text }); // <-- FIX: must be { text: "..." }
-
-      // await client.sendMessage(
-      //   number,
-      //   ` Lampu ${topic} : ${message.toString()}`
-      // );
+      await client.sendMessage(jid, { text });
       console.log(`✅ Sent to ${jid}: ${text}`);
-      //   console.log(`✅ Message sent to ${number}`);
     } catch (err) {
-      console.error(`❌ Failed to send to :`, err);
+      console.error(`❌ Failed to send to:`, err);
     }
   }
 }
