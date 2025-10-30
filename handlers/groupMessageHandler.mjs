c477ca210fea77c710014c9a5eb17d78c362e8ca
Yudhistira123 <yudhistira.sulaeman@gmail.com>
2025-10-30 14:59:10 +0700

    ss

--- handlers/groupMessageHandler.mjs
+++ handlers/groupMessageHandler.mjs
@@ -33,7 +33,7 @@ export default async function groupMessageHandler(client, message) {
     let topic = parts[2]; // keep original case
     let isi = parts[3]; // keep original case
     if (isi !== undefined) {
-      isi = "";
+      isi = "0";
     }
 
     if (topic === undefined) {
