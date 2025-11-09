9f85b7a0304853ca7b4d551574b5b8935c24ce06
Yudhistira123 <yudhistira.sulaeman@gmail.com>
2025-11-09 10:07:49 +0700

    x

--- utils/attendance.js
+++ utils/attendance.js
@@ -47,7 +47,27 @@ async function getDistance(lat1, lon1, lat2, lon2) {
     return msg;
   } catch (err) {
     console.error("❌ Error:", err.message || err);
+    const distanceKm = (haversineDistance(start, end) / 1000).toFixed(2);
+    const msg = `🏍️ to Musashi: ${distanceKm} km 🏁`;
+    return msg;
   }
+}
+
+function haversineDistance(lat1, lon1, lat2, lon2) {
+  const R = 6371e3; // Earth radius in meters
+  const toRad = (x) => (x * Math.PI) / 180;
+
+  const φ1 = toRad(lat1);
+  const φ2 = toRad(lat2);
+  const Δφ = toRad(lat2 - lat1);
+  const Δλ = toRad(lon2 - lon1);
+
+  const a =
+    Math.sin(Δφ / 2) ** 2 + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) ** 2;
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return R * c; // distance in meters
 }
 
 // helper untuk ambil nomor WA dari JID
